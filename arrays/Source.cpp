9ca03afd951116a8776c74433db5e7d050356e22
Tihom_lg74@top-academy.ru <Tihom_lg74@top-academy.ru>
2023-03-15 15:47:04 +0300

    ShiftLeftRight

--- arrays/Source.cpp
+++ arrays/Source.cpp
@@ -1,11 +1,12 @@
 ﻿#include <iostream>;
 using namespace std;
-//#define RIGHT
-#define LEFT
+void ShiftLeft(int arr[],int n);
+void ShiftRight(int arr[], int n);
+//#define MIN_MAX
 void main()
 {
 	setlocale(LC_ALL, "");
-	const int n = 15;
+	const int n = 10;
 	int arr[n];
 	//cout << "Введите элементы массива: ";
 	//for (int i = 0; i < n; i++)
@@ -14,7 +15,8 @@ void main()
 	//}
 	//cout << endl;
 	//rand(); // rand возвращает псевдослучайное число в диапазоне от 0 до 32677 (MAX_RAND)
-	
+
+#ifdef	MIN_MAX;
 	for (int i = 0; i < n; i++)
 	{
 		arr[i] = rand()%100;
@@ -36,35 +38,73 @@ void main()
 	cout << "Среднее арифметическое всех элементовмассива: " << double(sum) / n << "\n";
 	cout << "Минимальный элемент массива: " << min << "\n";
 	cout << "Максимальный элемент массива: " << max << "\n";
-	int m = 0;
-	int buf;
+#endif;
 	
-#ifdef RIGHT;
+	ShiftRight(arr, n);
+	cout << "\n";
+	for (int i = 0; i < n; i++)
+	{
+		cout << "--------"; 
+	}
+	cout << "\n";
+	ShiftLeft(arr, n);
+}
+void ShiftRight(int arr[], int n)
+{		
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = i;// rand() % 100;
+	}
+	cout << "\nИсходный массив:\n";
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << "\t";
+	}
+	cout << endl;
+	int buf = 0 , m;
 	cout << "Введите число, на которое нужно сдвинуть элементы массива вправо: "; cin >> m;
-	for (int i = 0; i < n-m; i++)
+	for (int i = 1; i <= m; i++)
 	{
-		buf = arr[i];
-		arr[i] = arr[m+i];
-		arr[m+i] = buf;
+		buf = arr[n-1];
+		for (int j = n-1; j > 0; j--)
+		{
+			arr[j] = arr[j-1];
+		}
+		arr[0] = buf;
 	}
+	cout << "\nМассив после сдвига:\n";
 	for (int i = 0; i < n; i++)
 	{
 		cout << arr[i] << "\t";
 	}
-#endif;	
-#ifdef LEFT;
+}
+void ShiftLeft(int arr[],int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = i;//rand() % 100;
+	}
+	cout << "\nИсходный массив:\n";
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << "\t";
+	}
+	cout << endl;
+	int buf = 0, m;
 	cout << "Введите число, на которое нужно сдвинуть элементы массива влево: "; cin >> m;
-	for (int i = m; i < n; i++)
+	for (int i = 1; i <= m; i++)
 	{
-		buf = arr[i-m];
-		arr[i-m] = arr[i];
-		arr[n - m] = buf;
+		buf = arr[0];
+		for (int j = 1; j < n; j++)
+		{
+			arr[j - 1] = arr[j];
+		}
+		arr[n - 1] = buf;
 	}
+	cout << "\nМассив после сдвига:\n";
 	for (int i = 0; i < n; i++)
 	{
 		cout << arr[i] << "\t";
 	}
-#endif;	
-
 }
 
